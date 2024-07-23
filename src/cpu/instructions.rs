00cfa4801ed3fdd68bcd6546383c7ac4bfa71d5b
Alex Wyatt <alexander@jdwyatt.com>
2024-07-22 20:24:32 -0400

    messy cpu work in progress

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,8 +1,24 @@
 
-fn instruction_handler(instruction : u16) -> u16 {
+fn opcode_handler(opcode : u16) -> u16 {
 
-    case instruction {
-        _ => "SHIT WENT WRONG"
+    match opcode {
+        0x00 => 1,
+        0x01 => 1,
+        0x02 => 1,
+        0x03 => 1,
+        0x04 => 1,
+        0x05 => 1,
+        0x06 => 1,
+        0x07 => 1,
+        0x08 => 1,
+        0x09 => 1,
+        0x0A => 1,
+        0x0B => 1,
+        0x0C => 1,
+        0x0D => 1,
+        0x0E => 1,
+        0x0F => 1,
+        _    => 0,
     }
-    return 
+    // return 10
 }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,8 +1,24 @@
+mod instructions;
+
+
 pub enum Flag {
     Z = 0x0080,
     N = 0x0040,
     H = 0x0020,
     C = 0x0010,
+}
+
+pub enum Register{
+    A,
+    F,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+    SP,
+    PC,
 }
 
 #[derive(Debug)]
@@ -35,6 +51,36 @@ impl Registers {
     pub fn clear_flag(&mut self, flag: Flag) {
         self.AF &= (flag as u16) ^ 0xFFFF;
     }
+
+    pub fn get_register(&mut self, value: u16, register: Register) -> u16{
+        match register {
+            Register::A => (self.AF >> 8),
+            Register::F => self.AF,
+            Register::B => (self.BC >> 8),
+            Register::C => self.BC,
+            Register::D => (self.DE >> 8),
+            Register::E => self.DE,
+            Register::H => (self.HL >> 8),
+            Register::L => self.HL,
+            Register::SP => self.SP,
+            Register::PC => self.PC,
+        }
+    }
+
+    pub fn set_register(&mut self, value: u16, register: Register) -> (){
+        match register {
+            Register::A => self.AF = (self.AF & 0x00FF) | value,
+            Register::F => self.AF = (self.AF & 0xFF00) | value,
+            Register::B => self.BC = (self.BC & 0x00FF) | value,
+            Register::C => self.BC = (self.BC & 0xFF00) | value,
+            Register::D => self.DE = (self.DE & 0x00FF) | value,
+            Register::E => self.DE = (self.DE & 0x00FF) | value,
+            Register::H => self.HL = (self.DE & 0x00FF) | value,
+            Register::L => self.HL = (self.DE & 0xFF00) | value,
+            Register::SP => self.SP = value,
+            Register::PC => self.PC = value,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -55,15 +101,10 @@ impl CPU {
     pub fn execute(&mut self, instruction : u16) {
 
     }
-}
 
-fn instruction_handler(instruction : u16) -> u16 {
+    //Provide debug info for TUI
+    pub fn get_state(&mut self) {
 
-    let opcode: u16 = (instruction & 0xF000) >> 12;
 
-    match instruction {
-        0x77 => 1023,
-        _    => 1023
     }
-     
 }
